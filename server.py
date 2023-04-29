d747c046fddd7a3d65773dbf2b0711dcdffaf62d
Natan <2niknatan@gmail.com>
2023-04-29 15:03:33 +0300

    update the server

--- server.py
+++ server.py
@@ -3,11 +3,8 @@ import re
 import mysql.connector
 from flask import Flask, request
 import json
-from difflib import SequenceMatcher
 import jellyfish
-import nltk
-from nltk import RegexpTokenizer
-from nltk.tokenize import word_tokenize
+
 
 app = Flask(__name__)
 host = "localhost"
@@ -54,31 +51,42 @@ def get_similar_items():
     product_name = query_parameters["name"]
     chain_id = query_parameters["chain_id"]
     my_cursor.execute(f'select * from product where chain_name != "{chains_id[chain_id]}"')
-
     fetched_data = my_cursor.fetchall().copy()
-    if " " not in product_name:
-        for row in fetched_data:
-            tokens = word_tokenize(row[3])
-            if product_name in tokens:
-                print(tokens)
+    product_dic = {}
+    for row in fetched_data:
+        if jellyfish.jaro_similarity(row[3], product_name) > 0.75:
+            product_dic[row[3]] = jellyfish.jaro_similarity(row[3], product_name)
 
-    else:
-        test_dic = {}
-        for row in fetched_data:
-            if jellyfish.jaro_similarity(row[3], product_name) > 0.75:
-                # print(jellyfish.jaro_similarity(row[3], product_name), "->", row[3])
-                test_dic[row[3]] = jellyfish.jaro_similarity(row[3], product_name)
+    sorted_dict = dict(sorted(product_dic.items(), key=lambda x: x[1], reverse=True))
+    res = {}
+    i = 0
+    for key in sorted_dict:
+        if i == 3:
+            break
+        my_cursor.execute(f'select item_price from product where item_name="{key}"')
+        fetched_data = my_cursor.fetchall().copy()
+        res[key] = fetched_data[0][0]
+        i += 1
+    print(res)
 
-        sorted_dict = dict(sorted(test_dic.items(), key=lambda x: x[1], reverse=True))
-        for key in sorted_dict:
-            print(key, "->", sorted_dict[key])
-
-    # print(len(sorted_data))
-    # for row in sorted_data:
-    #     if jellyfish.jaro_similarity(row[3], product_name) > 0.7:
-    #         print(jellyfish.jaro_similarity(row[3], product_name), "->", row[3], i)
-
-    return "hello"
+    return json.dumps(res)
+    # if " " not in product_name:
+    #     for row in fetched_data:
+    #         tokens = word_tokenize(row[3])
+    #         if product_name in tokens:
+    #             print(tokens)
+    #
+    # else:
+    #     test_dic = {}
+    #     for row in fetched_data:
+    #         if jellyfish.jaro_similarity(row[3], product_name) > 0.75:
+    #             print(jellyfish.jaro_similarity(row[3], product_name), "->", row[3])
+    #             test_dic[row[3]] = jellyfish.jaro_similarity(row[3], product_name)
+    #
+    #     sorted_dict = dict(sorted(test_dic.items(), key=lambda x: x[1], reverse=True))
+    #     for key in sorted_dict:
+    #         print(key, "->", sorted_dict[key])
+    #
 
 
 if __name__ == '__main__':
--- sql_queries/create_DB.sql
+++ sql_queries/create_DB.sql
@@ -1,22 +1,18 @@
-create database Carting_DB;
+-- create database Carting_DB;
 use Carting_DB;
 CREATE TABLE product (
     chain_name VARCHAR(50),
-	price_update_date date,
+	price_update_date VARCHAR(50),
     item_code VARCHAR(50),
-    item_type varchar(50),
     item_name varchar(200),
-    manufacturer_name varchar(50),
-    manufacture_country varchar(50),
     manufacturer_item_description varchar(100),
-    unity_qty integer,
+    unity_qty varchar(50),
     quantity integer,
-    b_is_weighted boolean,
     unit_of_measure varchar(50),
     qty_in_package float,
     item_price float,
     unit_of_measure_price float,
-    item_status bool,
+    item_status varchar(10),
     PRIMARY KEY (item_code, chain_name)
 )
 DEFAULT CHARACTER SET utf8
