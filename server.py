91e5fb0c43613d9207b8b488826736d2564a64df
Alon <alon.hasson@auroralabs.com>
2023-07-29 12:17:15 +0300

    add cache

--- server.py
+++ server.py
@@ -15,7 +15,11 @@ chains_id = {'1': 'rami_levi',
              '2': 'shufersal'}
 
 product_name = ""
-openai.api_key = r'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'
+openai.api_key = r'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'
+
+similar_cache = {}
+
+logging.basicConfig(level=logging.INFO)
 
 
 # Function to create a database connection and cursor
@@ -23,7 +27,7 @@ def get_database_connection():
     db_config = {
         'host': 'localhost',
         'user': 'root',
-        'password': '2nik',
+        'password': '123456',
         'database': 'Carting_DB',
         'port': 3306
     }
@@ -68,8 +72,6 @@ def get_product_by_id(cursor):
     chain_name = chains_id[query_parameters["chain_id"]]
     product_serial_number = query_parameters["product_id"]
 
-    logging.info(f' going to execute query: select item_price, item_name, chain_name from product where item_code '
-                 f'LIKE "%{product_serial_number}%" AND chain_name != "{chain_name}"')
     cursor.execute(
         f'select item_price, item_name, chain_name from product where item_code LIKE "%{product_serial_number}"'
         f'AND chain_name != "{chain_name}"')
@@ -89,43 +91,13 @@ def get_product_by_id(cursor):
 
 
 @app.route("/product/name")
-@with_cursor
-def get_similar_items(cursor):
+def api_product_by_name():
     global product_name
     query_parameters = request.args
     product_name = query_parameters["name"]
     chain_id = query_parameters["chain_id"]
     try:
-        logging.info(f'going to execute: select * from product where chain_name != "{chains_id[chain_id]}"')
-        cursor.execute(f'select item_name from product where chain_name != "{chains_id[chain_id]}"')
-        fetched_data = cursor.fetchall().copy()
-        product_dic = {}
-        for row in fetched_data:
-            product_dic[row[0]] = jellyfish.jaro_similarity(row[0], product_name)
-        sorted_dict = dict(sorted(product_dic.items(), key=lambda x: x[1], reverse=True)[:10])
-        f = open('question.txt', 'r', encoding='utf-8')
-        message = f.read()
-        message = message.replace('<<>>', product_name)
-        for key in sorted_dict:
-            message += f'\n{key}'
-        logging.info(message)
-        response = openai.ChatCompletion.create(
-            model="gpt-4",
-            messages=[{'role': 'system', 'content': 'You are a helpful assistant.'},
-                      {'role': 'user', 'content': message}]
-        )
-        generated_text = response.choices[0].message.content
-        dict_text = json.loads(cut_string_between_brackets(generated_text))
-        res = []
-        for key in dict_text:
-            temp_dict = {}
-            logging.info(f"select item_price, item_code from product where item_name='{dict_text[key]}'")
-            cursor.execute(f"select item_price, item_code from product where item_name='{dict_text[key]}'")
-            fetched_data = cursor.fetchall().copy()
-            temp_dict['item_name'] = dict_text[key]
-            temp_dict['item_price'] = fetched_data[0][0]
-            temp_dict['item_code'] = fetched_data[0][1]
-            res.append(temp_dict)
+        res = get_similar_items(product_name, chain_id)
         return json.dumps(res)
     except Exception as e:
         logging.error(f'error in query: select * from product where chain_name != "{chains_id[chain_id]}"')
@@ -144,5 +116,45 @@ def cut_string_between_brackets(input_string):
         return None
 
 
+@with_cursor
+def get_similar_items(product_name, chain_id, cursor):
+    cache_key = f"{product_name}-{chain_id}"
+    if cache_key in similar_cache:
+        logging.info(f"found {cache_key} in cache")
+        return similar_cache[cache_key]
+
+    logging.warning(f"didnt find {cache_key} in cache")
+    cursor.execute(f'select item_name from product where chain_name != "{chains_id[chain_id]}"')
+    fetched_data = cursor.fetchall().copy()
+    product_dic = {}
+    for row in fetched_data:
+        product_dic[row[0]] = jellyfish.jaro_similarity(row[0], product_name)
+    sorted_dict = dict(sorted(product_dic.items(), key=lambda x: x[1], reverse=True)[:10])
+    f = open('question.txt', 'r', encoding='utf-8')
+    message = f.read()
+    message = message.replace('<<>>', product_name)
+    for key in sorted_dict:
+        message += f'\n{key}'
+    response = openai.ChatCompletion.create(
+        model="gpt-4",
+        messages=[{'role': 'system', 'content': 'You are a helpful assistant.'},
+                  {'role': 'user', 'content': message}]
+    )
+    generated_text = response.choices[0].message.content
+    dict_text = json.loads(cut_string_between_brackets(generated_text))
+    res = []
+    for key in dict_text:
+        temp_dict = {}
+        cursor.execute(f"select item_price, item_code from product where item_name='{dict_text[key]}'")
+        fetched_data = cursor.fetchall().copy()
+        temp_dict['item_name'] = dict_text[key]
+        temp_dict['item_price'] = fetched_data[0][0]
+        temp_dict['item_code'] = fetched_data[0][1]
+        res.append(temp_dict)
+
+    res = sorted(res, key=lambda x: x['item_price'])
+    logging.info(f"cache updated with key = {cache_key}")
+    similar_cache[cache_key] = res
+    return res
 if __name__ == '__main__':
     app.run(host=host, port=port)
